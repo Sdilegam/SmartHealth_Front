8c1ad6090420a048f7d72b7d5cd58cf79cc9f9d2
salva <83271357+Sdilegam@users.noreply.github.com>
2025-05-05 06:11:58 +0200

    Small import error correction

--- src/app/guards/authenticatedGuard.ts
+++ src/app/guards/authenticatedGuard.ts
@@ -1,6 +1,6 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
-import {SessionService} from '../../../../../DigitalCity2025-IntermediateLabo/IntermediateLabo-Frontend/src/app/services/session.service';
+import {SessionService} from '../services/session.service';
 
 export const authenticatedGuard: CanActivateFn = (route, state) => {
   if (inject(SessionService).session().token){
